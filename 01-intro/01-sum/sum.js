e058a693cef0acd522819a95fb836dbcc5388e1e
user <y.dubovitsky@gmail.com>
2022-01-19 10:54:56 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,8 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('args no valid, please insert only number values!');
+  }
+  return a + b;
 }
 
 module.exports = sum;
